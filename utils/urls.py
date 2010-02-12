074632ebb85869b3b8aed052f16b5e4373ed6c77
timjdavey <timjdavey@gmail.com>
2010-02-12 02:20:17 +0000

    random secrets now at /inspire/

--- utils/urls.py
+++ utils/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
+    url(r'^inspire/$', random_secret, name='random_secret'),
     url(r'^$', home, name='home'),
 )
--- utils/views.py
+++ utils/views.py
@@ -1,7 +1,16 @@
 from django.conf import settings
+from django.http import HttpResponseRedirect
 from secret.models import Secret
 from discussion.models import Discussion
 from utils.shortcuts import context_response
+
+
+def random_secret(request):
+    " Redirects you to a random secret "
+    import random
+    ids = Secret.viewable.values_list('id', flat=True)
+    choice = random.choice(ids)
+    return HttpResponseRedirect(Secret.objects.get(pk=choice).get_absolute_url())
 
 
 def home(request):
