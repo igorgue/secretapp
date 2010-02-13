125cad2d94ee191c07449d84fcf6c7685d51c1e5
timjdavey <timjdavey@gmail.com>
2010-02-13 10:22:41 +0000

    FIXED?

--- settings.py
+++ settings.py
@@ -67,7 +67,7 @@ USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '%s/templates/static/'
+MEDIA_ROOT = '%s/templates/static/' % CWD
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
