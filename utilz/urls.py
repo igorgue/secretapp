8446e2d058dc7a0aa947d8b94e13c996ee7af655
Matthew Pontefract <mp@angelina.(none)>
2010-02-16 07:23:59 +0000

    get_flagspam_url method put on UserContent; urlcache middleware commented out pending bug fix

--- utilz/urls.py
+++ utilz/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('',
     url(r'^inspire/$', random_secret, name='random_secret'),
     url(r'^stats/$', stats, name='stats'),
     url(r'^$', home, name='home'),
-    url(r'^flagspam/(?P<modelid>\d+)/(?P<objectid>\d+)/$', flag_spam),
     url(r'^(?P<template>\w+)/$', render, name='render_template'),
 )
