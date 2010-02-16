81f1999fbe94f859dc13592288033003bb85098a
Matthew Pontefract <mp@angelina.(none)>
2010-02-16 00:32:42 +0000

    minor minor mod to ajax middleware that saves duplication of a test in the code

--- utilz/middleware.py
+++ utilz/middleware.py
@@ -3,17 +3,16 @@ from django.http import HttpResponseServerError
 
 class AjaxExceptionResponse(object):
    def process_exception(self, request, exception):
-       if settings.DEBUG:
-           if request.is_ajax():
-               import sys, traceback
-               (exc_type, exc_info, tb) = sys.exc_info()
-               response = "%s\n" % exc_type.__name__
-               response += "%s\n\n" % exc_info
-               response += "TRACEBACK:\n"
-               for tb in traceback.format_tb(tb):
-                   response += "%s\n" % tb
-               return HttpResponseServerError(response)
-       else:
-           if request.is_ajax():
-               return HttpResponseServerError('500')
+       if request.is_ajax():
+           if settings.DEBUG:
+                   import sys, traceback
+                   (exc_type, exc_info, tb) = sys.exc_info()
+                   response = "%s\n" % exc_type.__name__
+                   response += "%s\n\n" % exc_info
+                   response += "TRACEBACK:\n"
+                   for tb in traceback.format_tb(tb):
+                       response += "%s\n" % tb
+                   return HttpResponseServerError(response)
+           else:
+                   return HttpResponseServerError('500')
 
