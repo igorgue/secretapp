085558c9dddfc684d6eeec19218e914a24411159
timjdavey <timjdavey@gmail.com>
2010-02-16 14:38:22 +0000

    fixing admin?

--- settings.py
+++ settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = '/static'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '%s/media' % MEDIA_URL
+ADMIN_MEDIA_PREFIX = '%s/media/' % MEDIA_URL
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
