c1cdd15e8efbd7e83443257eb35e1833dcc103af
timjdavey <timjdavey@gmail.com>
2010-02-19 20:04:19 +0000

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,7 @@ class UserSettingsForm(forms.Form):
     def save(self, *args, **kwargs):
         # settings file
         if 'publish_to_wall' in self.cleaned_data:
-            us = UserSettings.objects.get_or_create(user=self.user)
+            us, new = UserSettings.objects.get_or_create(user=self.user)
             us.publish_to_wall = self.cleaned_data['publish_to_wall']
             us.save()
         # user profile
