2eb9b0f9ec71edc9889f32de59a4c7deff485b41
timjdavey <timjdavey@gmail.com>
2010-02-19 19:52:32 +0000

    brain candy

--- utilz/urls.py
+++ utilz/urls.py
@@ -4,6 +4,6 @@ from views import *
 urlpatterns = patterns('',
     url(r'^inspire/$', random_secret, name='random_secret'),
     url(r'^$', home, name='home'),
-    url(r'^alt/$', alt_home, name='home'),
+    url(r'^alt/$', alt_home, name='alt_home'),
     url(r'^(?P<template>\w+)/$', render, name='render_template'),
 )
