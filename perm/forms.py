a5dd515eaab1246c07722ac1c8675edec677699d
timjdavey <timjdavey@gmail.com>
2010-02-22 11:48:32 +0000

    balls it

--- perm/forms.py
+++ perm/forms.py
@@ -8,13 +8,13 @@ class UserContentForm(forms.ModelForm):
         self.Meta.exclude = ('approved',)
         
         if permission_level > 2:
-            if 'instance' in kwargs:
+            try:
                 instance = kwargs['instance']
                 user = instance.created_by
                 self.use_facebook = True
                 self.fields['facebook_uid'] = forms.CharField(initial=user.username.replace('FB:', ''), required=False, help_text="Right click on the user name. Copy link address. Paste here.")
                 self.fields['facebook_name'] = forms.CharField(initial=user.first_name, required=False, help_text="Write the firstname or initials of user here.")
-            else:
+            except:
                 self.use_facebook = True
                 self.fields['facebook_uid'] = forms.CharField(required=False, help_text="Right click on the user name. Copy link address. Paste here.")
                 self.fields['facebook_name'] = forms.CharField(required=False, help_text="Write the firstname or initials of user here.")
