6700fd09dd2053df2c1dfa39b72f373f54ae1e89
timjdavey <timjdavey@gmail.com>
2010-03-11 12:27:29 +0000

    making messages now send as part of a single connection

--- communication/management/commands/send_unsent_mail.py
+++ communication/management/commands/send_unsent_mail.py
@@ -1,15 +1,31 @@
 from django.core.management.base import NoArgsCommand
 from communication.models import Communication
-
-class FailedToSendMail(Exception):
-    pass
+from django.core import mail
 
 
 class Command(NoArgsCommand):
     help = "Emit queued notices."
     
     def handle_noargs(self, **options):
-        successful = Communication.objects.send_unsent()
-        if not successful:
-            import datetime
-            raise FailedToSendMail("Failed to send some messages -- see admin for failed messages")
+        # open mail connection
+        connection = mail.SMTPConnection()
+        
+        # collect all unsent mail
+        unsent_coms = Communication.objects.unsent()
+        messages = []
+        
+        # build a list of all mail to be sent
+        for u in unsent_coms:
+            if u.should_send():
+                messages.append(u.email_message())
+            else:
+                u.deleted = True
+                u.save()
+        
+        # try sending and update status of messages
+        try:
+            connection.send_messages(messages)
+        except:
+            unsent_coms.update(failed=True)
+        else:
+            unsent_coms.update(sent=True)
